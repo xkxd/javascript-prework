4b49fe1a3b72017ba76dc4f5b0619956888f92a9
Paulina Szeszel <paulina_szeszel@o2.pl>
2021-07-16 19:16:01 +0200

    add variable js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ function printMessage(msg){
 function printMessage(msg){
   printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
 }
+
+let computerMove = `kamień`;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = `papier`;
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
