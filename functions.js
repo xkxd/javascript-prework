70f2500b9868137573f2bb25e4d6646bf252264e
Paulina Szeszel <paulina_szeszel@o2.pl>
2021-07-16 18:58:42 +0200

    add variable js

---
+++ functions.js
@@ -0,0 +1,8 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
---
+++ js/script.js
@@ -0,0 +1,6 @@
+function printMessage(msg){
+  printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+}
+function printMessage(msg){
+  printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+}
