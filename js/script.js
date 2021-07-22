0756f427682f04446c21b1060099352f463b4708
Paulina Szeszel <paulina_szeszel@o2.pl>
2021-07-22 16:32:48 +0200

    add script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'papier';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+if(randomNumber == 2){
+  computerMove = 'papier';
+}
+
+if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
