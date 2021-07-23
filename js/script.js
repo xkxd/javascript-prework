beb6602b9e617b620040c75a369bc655cb668420
Paulina Szeszel <paulina_szeszel@o2.pl>
2021-07-23 15:01:34 +0200

    add lines 29-51 script.js

--- js/script.js
+++ js/script.js
@@ -25,3 +25,27 @@ if(1 < 3){
 let computerMove = `papier`;
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if(1 < 2){
+  printMessage('Super! Papier owinął kamień');
+} else if (1 > 2) {
+  printMessage('Przegrywasz! Spróbuj jeszcze raz!');
+}
+
+if(1 < 3){
+  printMessage('O nie! Nożyce się stępiły!');
+} else if (1 > 3) {
+  printMessage('Super! Wygrywasz!');
+}
