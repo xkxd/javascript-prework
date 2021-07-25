77c924767306b4444d37029e393747a39f711b12
Paulina Szeszel <paulina_szeszel@o2.pl>
2021-07-25 13:43:34 +0200

    add lines 8-23 script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ let computerMove = 'nieznany ruch';
 if (randomNumber === 1) {computerMove = 'kamień'; }
 else if (randomNumber === 2) {computerMove = 'papier'; }
 else if (randomNumer === 3) {computerMove = 'nożyce'; }
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if (playerInput == '2'){
+  playerMove = 'papier';
+}
+else if (playerInput == '3'){
+  playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
