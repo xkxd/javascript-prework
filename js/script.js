474c84292e393c2849ec9124eac5bb77ab957057
Paulina Szeszel <paulina_szeszel@o2.pl>
2021-07-25 14:13:03 +0200

    add lines 25-51 script.js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,31 @@ else if (playerInput == '3'){
   playerMove = 'nożyce';
 }
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Przegrywasz!');
+}
+else if (computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+else if (computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+else if (computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Przegrywasz!');
+}
+else if (computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else if (computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Przegrywasz!');
+}
+else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
