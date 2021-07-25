d62dfb99d70f6fb38c8bc87e8a8836714b24316d
Paulina Szeszel <paulina_szeszel@o2.pl>
2021-07-25 13:34:47 +0200

    changed all of script.js

--- js/script.js
+++ js/script.js
@@ -1,57 +1,6 @@
-let randomFraction = Math.random();
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Wylosowany ułamek to: ' + randomFraction);
-
-let calculation = randomFraction * 3 + 1;
-
-printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
-
-let roundNumber = Math.floor(calculation);
-
-printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
-
-if(1 < 2){
-  printMessage('Super! Papier owinął kamień');
-} else if (1 > 2) {
-  printMessage('Przegrywasz! Spróbuj jeszcze raz!');
-}
-
-if(1 < 3){
-  printMessage('O nie! Nożyce się stępiły!');
-} else if (1 > 3) {
-  printMessage('Super! Wygrywasz!');
-}
-
-let computerMove = `papier`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}
-
-printMessage('Twój ruch to: ' + playerMove);
-
-if(1 < 2){
-  printMessage('Super! Papier owinął kamień');
-} else if (1 > 2) {
-  printMessage('Przegrywasz! Spróbuj jeszcze raz!');
-}
-
-if(1 < 3){
-  printMessage('O nie! Nożyce się stępiły!');
-} else if (1 > 3) {
-  printMessage('Super! Wygrywasz!');
-}
-
-if( computerMove == 'papier' && playerMove == 'kamień'){
-  printMessage('Ty wygrywasz!');
-} else if (computerMove == computerMove) {
-  printMessage('Remis!');
-}
+let computerMove = 'nieznany ruch';
+if (randomNumber === 1) {computerMove = 'kamień'; }
+else if (randomNumber === 2) {computerMove = 'papier'; }
+else if (randomNumer === 3) {computerMove = 'nożyce'; }
